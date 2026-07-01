0194dcb1510040a24d5ffb08e5200ca674372e60
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-07-01 14:00:03 +0200

    Split out build action

---
+++ src/CompilerTestBuildfile.elm
@@ -0,0 +1,162 @@
+module CompilerTestBuildfile exposing (Package, buildAction, getInput)
+
+import BackendTask exposing (BackendTask)
+import BackendTask.Http as Http
+import BuildTask exposing (BuildTask, FileOrDirectory)
+import BuildTask.Tar
+import BuildTask.Unsafe.Do
+import FatalError exposing (FatalError)
+import Json.Decode
+import Json.Encode
+import Path
+import Url.Builder
+
+
+type alias Package =
+    { author : String
+    , name : String
+    , version : String
+    }
+
+
+buildAction : List Package -> BuildTask FileOrDirectory
+buildAction packages =
+    let
+        packagesCount : Int
+        packagesCount =
+            List.length packages
+    in
+    packages
+        |> List.indexedMap
+            (\index package ->
+                let
+                    prefix : String
+                    prefix =
+                        "["
+                            ++ String.padLeft (String.length (String.fromInt packagesCount)) '0' (String.fromInt index)
+                            ++ "/"
+                            ++ String.fromInt packagesCount
+                            ++ "]"
+                in
+                downloadPackage package
+                    |> BuildTask.withPrefix prefix
+                    |> BuildTask.map
+                        (\( downloaded, hasTests ) ->
+                            { filename = Path.path (String.join "/" [ package.author, package.name, package.version ])
+                            , hash = downloaded
+                            }
+                        )
+            )
+        |> BuildTask.combine
+        |> BuildTask.andThen BuildTask.combineInto
+
+
+getInput : BackendTask FatalError (List Package)
+getInput =
+    -- 0.19 cutoff
+    Http.get "https://package.elm-lang.org/all-packages/since/6557"
+        (Http.expectJson packageListDecoder)
+        |> BackendTask.allowFatal
+
+
+packageListDecoder : Json.Decode.Decoder (List Package)
+packageListDecoder =
+    Json.Decode.string
+        |> Json.Decode.andThen
+            (\raw ->
+                case String.split "@" raw of
+                    [ before, version ] ->
+                        case String.split "/" before of
+                            [ author, name ] ->
+                                Json.Decode.succeed
+                                    { author = author
+                                    , name = name
+                                    , version = version
+                                    }
+
+                            _ ->
+                                let
+                                    msg : String
+                                    msg =
+                                        "Could not split package author and name in " ++ escape raw
+                                in
+                                Json.Decode.fail msg
+
+                    _ ->
+                        let
+                            msg : String
+                            msg =
+                                "Could not split package author/name and version in " ++ escape raw
+                        in
+                        Json.Decode.fail msg
+            )
+        |> Json.Decode.list
+
+
+downloadPackage : Package -> BuildTask ( FileOrDirectory, Bool )
+downloadPackage package =
+    let
+        targetFolder : String
+        targetFolder =
+            String.join "/"
+                [ "repos"
+                , package.author
+                , package.name
+                , package.version
+                ]
+
+        url : String
+        url =
+            Url.Builder.crossOrigin "https://github.com"
+                [ package.author
+                , package.name
+                , "archive"
+                , "refs"
+                , "tags"
+                , package.version ++ ".tar.gz"
+                ]
+                []
+
+        getRoot : List String -> BuildTask String
+        getRoot contents =
+            case contents of
+                firstLine :: _ ->
+                    case String.split "/" firstLine of
+                        [ root, "" ] ->
+                            BuildTask.succeed root
+
+                        _ ->
+                            BuildTask.fail ("Unexpected first content line: " ++ escape firstLine)
+
+                _ ->
+                    -- Empty file, root is irrelevant
+                    BuildTask.succeed ""
+    in
+    BuildTask.Unsafe.Do.downloadImmutable url <| \tarGz ->
+    BuildTask.Unsafe.Do.pipeThrough "gunzip" [] tarGz <| \tar ->
+    BuildTask.do (BuildTask.Tar.listContents tar) <| \contents ->
+    BuildTask.do (getRoot contents) <| \root ->
+    let
+        hasTests : Bool
+        hasTests =
+            List.member (root ++ "/tests/") contents
+    in
+    BuildTask.do
+        (if hasTests then
+            BuildTask.Tar.extract { stripPrefix = Just root } tar [ "elm.json", "src", "tests" ]
+
+         else
+            BuildTask.Tar.extract { stripPrefix = Just root } tar [ "elm.json", "src" ]
+        )
+    <| \result ->
+    BuildTask.succeed ( result, hasTests )
+
+
+escape : String -> String
+escape s =
+    Json.Encode.encode 0 (Json.Encode.string s)
+
+
+packageToString : Package -> String
+packageToString package =
+    package.author ++ "/" ++ package.name ++ ":" ++ package.version
--- src/Main.elm
+++ src/Main.elm
@@ -18,6 +18,7 @@ import BuildTask.Unsafe.Do
 import Cli.Option as Option
 import Cli.OptionsParser as OptionsParser
 import Cli.Program as Program
+import CompilerTestBuildfile exposing (Package)
 import FastSet as Set exposing (Set)
 import FatalError exposing (FatalError)
 import Hash
@@ -27,28 +28,7 @@ import List.Extra
 import Pages.Script as Script exposing (Script, makeDirectory)
 import Path exposing (Path)
 import Time
-import Tui
-import Tui.Effect as Effect exposing (Effect)
-import Tui.Screen as Screen exposing (Screen)
-import Tui.Sub
 import Url.Builder
-
-
-type DownloadError
-    = FailedToRemoveDirectory String
-    | FailedToRemoveFile String
-    | FailedToMakeDirectory String
-    | FailedToExecute String
-    | FailedToStatFile String
-    | DownloadedEmptyFile String String
-    | FailedToExtractRootFolder String
-
-
-type alias Package =
-    { author : String
-    , name : String
-    , version : String
-    }
 
 
 run : Script
@@ -62,8 +42,8 @@ programConfig =
         |> Program.add
             (OptionsParser.build
                 (Config
-                    (BackendTask.allowFatal getPackageList)
-                    buildAction
+                    CompilerTestBuildfile.getInput
+                    CompilerTestBuildfile.buildAction
                 )
                 |> OptionsParser.with
                     (Option.optionalKeywordArg "build"
@@ -229,145 +209,3 @@ timeToString ms =
 
     else
         String.fromInt ms ++ "ms"
-
-
-buildAction : List Package -> BuildTask FileOrDirectory
-buildAction packages =
-    let
-        packagesCount : Int
-        packagesCount =
-            List.length packages
-    in
-    packages
-        |> List.indexedMap
-            (\index package ->
-                let
-                    prefix : String
-                    prefix =
-                        "["
-                            ++ String.padLeft (String.length (String.fromInt packagesCount)) '0' (String.fromInt index)
-                            ++ "/"
-                            ++ String.fromInt packagesCount
-                            ++ "]"
-                in
-                downloadPackage package
-                    |> BuildTask.withPrefix prefix
-                    |> BuildTask.map
-                        (\( downloaded, hasTests ) ->
-                            { filename = Path.path (String.join "/" [ package.author, package.name, package.version ])
-                            , hash = downloaded
-                            }
-                        )
-            )
-        |> BuildTask.combine
-        |> BuildTask.andThen BuildTask.combineInto
-
-
-getPackageList : BackendTask { fatal : FatalError, recoverable : Http.Error } (List Package)
-getPackageList =
-    -- 0.19 cutoff
-    Http.get "https://package.elm-lang.org/all-packages/since/6557"
-        (Http.expectJson packageListDecoder)
-
-
-packageListDecoder : Json.Decode.Decoder (List Package)
-packageListDecoder =
-    Json.Decode.string
-        |> Json.Decode.andThen
-            (\raw ->
-                case String.split "@" raw of
-                    [ before, version ] ->
-                        case String.split "/" before of
-                            [ author, name ] ->
-                                Json.Decode.succeed
-                                    { author = author
-                                    , name = name
-                                    , version = version
-                                    }
-
-                            _ ->
-                                let
-                                    msg : String
-                                    msg =
-                                        "Could not split package author and name in " ++ escape raw
-                                in
-                                Json.Decode.fail msg
-
-                    _ ->
-                        let
-                            msg : String
-                            msg =
-                                "Could not split package author/name and version in " ++ escape raw
-                        in
-                        Json.Decode.fail msg
-            )
-        |> Json.Decode.list
-
-
-downloadPackage : Package -> BuildTask ( FileOrDirectory, Bool )
-downloadPackage package =
-    let
-        targetFolder : String
-        targetFolder =
-            String.join "/"
-                [ "repos"
-                , package.author
-                , package.name
-                , package.version
-                ]
-
-        url : String
-        url =
-            Url.Builder.crossOrigin "https://github.com"
-                [ package.author
-                , package.name
-                , "archive"
-                , "refs"
-                , "tags"
-                , package.version ++ ".tar.gz"
-                ]
-                []
-
-        getRoot : List String -> BuildTask String
-        getRoot contents =
-            case contents of
-                firstLine :: _ ->
-                    case String.split "/" firstLine of
-                        [ root, "" ] ->
-                            BuildTask.succeed root
-
-                        _ ->
-                            BuildTask.fail ("Unexpected first content line: " ++ escape firstLine)
-
-                _ ->
-                    -- Empty file, root is irrelevant
-                    BuildTask.succeed ""
-    in
-    BuildTask.Unsafe.Do.downloadImmutable url <| \tarGz ->
-    BuildTask.Unsafe.Do.pipeThrough "gunzip" [] tarGz <| \tar ->
-    BuildTask.do (BuildTask.Tar.listContents tar) <| \contents ->
-    BuildTask.do (getRoot contents) <| \root ->
-    let
-        hasTests : Bool
-        hasTests =
-            List.member (root ++ "/tests/") contents
-    in
-    BuildTask.do
-        (if hasTests then
-            BuildTask.Tar.extract { stripPrefix = Just root } tar [ "elm.json", "src", "tests" ]
-
-         else
-            BuildTask.Tar.extract { stripPrefix = Just root } tar [ "elm.json", "src" ]
-        )
-    <| \result ->
-    BuildTask.succeed ( result, hasTests )
-
-
-escape : String -> String
-escape s =
-    Json.Encode.encode 0 (Json.Encode.string s)
-
-
-packageToString : Package -> String
-packageToString package =
-    package.author ++ "/" ++ package.name ++ ":" ++ package.version
