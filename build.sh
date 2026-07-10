2ded8941072d46ac57b9ca17a02ba8bbebe95e56
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-07-10 18:05:18 +0200

    Remove redundant space

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -e
-bun start -- --remove-stale 
+bun start -- --remove-stale
 ln -sf ../elm-lamdera-compilers-test-repos/$(readlink out | cut -d '/' -f2) out 
 cat out > out.xlsx 
 rm out
